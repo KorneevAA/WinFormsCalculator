ad1e4921501dfe3ab36350439ff1eac0e0f9a44e
StandShiba <korneev092005@yandex.ru>
2026-03-04 13:10:09 +0300

    Add InputProcessor

---
+++ IInputProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsCalculator
+{
+    internal interface IInputProcessor
+    {
+        string ProcessDecimalPoint(string currentInput);
+        string ProcessNumberInput(string currentInput, string newDigit);
+        string ProcessChangeSign(string currentInput);
+        string ProcessBackspace(string currentInput);
+    }
+}
---
+++ InputProcessor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsCalculator
+{
+    internal class InputProcessor : IInputProcessor
+    {
+        public string ProcessNumberInput(string currentInput, string newDigit)
+        {
+            if (currentInput == "" || currentInput == "0")
+                return newDigit;
+            else
+                return currentInput + newDigit;
+        }
+        public string ProcessDecimalPoint(string currentInput)
+        {
+            if (currentInput == "Ошибка")
+                return currentInput;
+            else if (!currentInput.Contains(","))
+                return currentInput + ",";
+            else
+                return currentInput;
+        }
+        public string ProcessChangeSign(string currentInput)
+        {
+            if (currentInput == "0" || currentInput == "Ошибка")
+                return currentInput;
+            else if (currentInput.StartsWith("-"))
+                return currentInput.Substring(1);
+            else
+                return "-" + currentInput;
+        }
+        public string ProcessBackspace(string currentInput)
+        {
+            if (currentInput == "Ошибка" || currentInput.Length <= 1)
+                return "0";
+
+            if (currentInput.Length == 2 && currentInput.StartsWith("-"))
+                return "0";
+
+            return currentInput.Substring(0, currentInput.Length - 1);
+        }
+    }
+}
